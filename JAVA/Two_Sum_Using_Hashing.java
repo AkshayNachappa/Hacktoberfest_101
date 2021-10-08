0ec96e36fd520ef71294680ff799972e0312d42f
Lalithya Manasa Patri <84711544+LalithyaManasaPatri@users.noreply.github.com>
2021-10-08 12:46:15 +0530

    Updated the code with relevant comments

--- JAVA/Two_Sum_Using_Hashing.java
+++ JAVA/Two_Sum_Using_Hashing.java
@@ -2,7 +2,14 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    static boolean printpairs(int arr[], int sum)
+    
+    //method to return if a pair exists or not in an array with the given sum
+    /*
+    Use a hash_map to check for the current array value x(let), if there exists a 
+    value target_sum-x which on adding to the former gives target_sum. 
+    This can be done in constant time. 
+    */
+    static boolean findpairs(int arr[], int sum)
     {
         HashSet<Integer> s = new HashSet<Integer>();
         boolean b=false;
@@ -20,14 +27,18 @@ public class Main {
     public static void main(String[] args)
     {
         Scanner in=new Scanner(System.in);
+        //input size of the array from the user
         int n= in.nextInt();
+        //Declaration of the array
         int A[] = new int[n];
+        //input Array elements from user
         for(int i=0;i<n;i++)
         {
             A[i]=in.nextInt();
         }
+        //input sum from the user
         int sum=in.nextInt();
-        if(printpairs(A, sum))
+        if(findpairs(A, sum))
             System.out.println("yes");
         else
             System.out.println("no");
