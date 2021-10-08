e107b5449ad8015c04dfb1be9571f7da0a385cf3
Akshay Nachappa <akshaynachappa@gmail.com>
2021-10-08 18:13:21 +0530

    Update Two_Sum_Using_Hashing.java

--- JAVA/Two_Sum_Using_Hashing.java
+++ JAVA/Two_Sum_Using_Hashing.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    
+
     //method to return if a pair exists or not in an array with the given sum
     /*
     Use a hash_map to check for the current array value x(let), if there exists a 
@@ -23,20 +23,22 @@ public class Main {
         }
         return b;
     }
-
     public static void main(String[] args)
     {
         Scanner in=new Scanner(System.in);
         //input size of the array from the user
+        System.out.println("Enter size of array: ");
         int n= in.nextInt();
         //Declaration of the array
         int A[] = new int[n];
+        System.out.println("Enter elements of the array: ");
         //input Array elements from user
         for(int i=0;i<n;i++)
         {
             A[i]=in.nextInt();
         }
         //input sum from the user
+        System.out.println("Enter the required sum to be checked: ");
         int sum=in.nextInt();
         if(findpairs(A, sum))
             System.out.println("yes");
