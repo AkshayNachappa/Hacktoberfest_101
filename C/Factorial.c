758313f89a8703a13477ab18882446cd03dfaac1
Deepak <60574055+11deepak0209@users.noreply.github.com>
2021-10-09 22:03:49 +0530

    Update Factorial.c

--- C/Factorial.c
+++ C/Factorial.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
+
+// recursive function 
+int fact(int num)
+{  if(num == 0)
+    {
+        return 1;
+    }
+   return num*fact(num-1);
+}
+// main function 
 void main()
-{
-	int i;
-	int n;
-	long factorial;
-	factorial=1;
-	printf("enter number to find its factorial \n");
-	scanf("%d",&n);
-	for(i=n;i>1;i--)
-	{
-		factorial*=i;
-	}
-	printf("factorial of %d is %d",n,factorial);
+{ 
+	int number =3 ;
+	printf("Enter the number to find factorial \n");
+	scanf("%d",&number);
+	printf("%d ",number);
+    
+    printf("factorial is %d",fact(number))  ;  
+
+    
 }
