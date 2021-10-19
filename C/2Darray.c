62f57f9711d53704d629a17eec0faa5e3b7df2e8
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-19 22:04:48 +0530

    Update 2Darray.c

--- C/2Darray.c
+++ C/2Darray.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main()
-{
+{ int i,j;
    int marks[4][6]={{13, 14, 15, 16, 17, 18},{1, 2, 3, 4, 5, 6},{20, 21, 22, 23, 24, 25},{7, 8, 9, 10, 11, 12}};
-   for(int i=0;i<4;i++)
+   for(i=0;i<4;i++)
    {
-       for (int j = 0; j < 6; j++)
-       {
+       for (j = 0; j < 6; j++)
            printf("%d ",marks[i][j]);
-       }
-       
    printf("\n");
    }
     return 0;
